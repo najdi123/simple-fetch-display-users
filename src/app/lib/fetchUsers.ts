cd82602b142894b844f0decb37731e45f9a2a701
monire91 <aglida1370@gmail.com>
2025-07-28 08:55:38 -0500

    fetched users

---
+++ src/app/lib/fetchUsers.ts
@@ -0,0 +1,14 @@
+type FetchUsersType={
+    id:number,
+    name:string,
+    email:string,
+}
+
+export async function fetchUsers() {
+    const res = await fetch('https://jsonplaceholder.typicode.com/users')
+    if (!res.ok){
+        throw new Error('Failed to fetch users')
+    }
+    return res.json()
+    
+}
